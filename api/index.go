4272ab9a081369eb28379b8eb2c2ff8197bd8df2
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-01-09 10:35:00 +0900

    first commit

---
+++ api/index.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/otiai10/opengraph"
+	"net/http"
+)
+
+type OGP struct {
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		http.Error(w, "query parameters error", http.StatusBadRequest)
+		return
+	}
+
+	ogp, err := opengraph.Fetch(url)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := OGP{ogp.Title, ogp.URL.Source, ogp.Description, ogp.Image[0].URL}
+
+	res, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, string(res))
+
+}
